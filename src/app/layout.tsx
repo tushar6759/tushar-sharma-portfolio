0a9038e998af129410c01cb054c8f98a43362a55
Tushar Sharma <tushar.sharma4@bajajfinserv.in>
2024-08-20 04:34:28 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,20 +52,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta charSet="UTF-8"/>
+        <link rel="icon" type="image/svg+xml" href="/img_1.png"/>
+        <title>Tushar Sharma</title>
+      </head>
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable
-        )}
+          className={cn(
+              "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+              fontSans.variable
+          )}
       >
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <TooltipProvider delayDuration={0}>
-            {children}
-            <Navbar />
-          </TooltipProvider>
-        </ThemeProvider>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <TooltipProvider delayDuration={0}>
+          {children}
+          <Navbar/>
+        </TooltipProvider>
+      </ThemeProvider>
       </body>
-    </html>
+      </html>
   );
 }
